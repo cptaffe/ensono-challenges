d4618f7ea8f968ff821a700e84823c7bc8e0401c
Connor Taffe <cpaynetaffe@gmail.com>
2016-04-24 23:28:10 -0500

    update & flex-box styling

--- contacts/app/app.component.ts
+++ contacts/app/app.component.ts
@@ -1,25 +1,12 @@
 import {Component} from 'angular2/core';
 import 'rxjs/add/operator/map'; // crappy shim
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
-import {Contact, ContactComponent} from './contact/contact.component';
+import {ContactList} from './contact-list/contact-list.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css'],
-    viewProviders: [HTTP_PROVIDERS],
-    directives: [ContactComponent]
+    directives: [ContactList]
 })
-export class AppComponent {
-  contacts: Contact[] = new Array<Contact>()
-  constructor(http: Http) {
-    http.get('seed.json')
-    .map(res => res.json())
-    .subscribe((contacts : {contacts: Contact[]}) => {
-      for (let c of contacts.contacts) {
-        this.contacts.push(c)
-      }
-      console.log(this.contacts)
-    })
-  }
-}
+export class AppComponent {}
---
+++ contacts/app/contact-list/contact-list.component.ts
@@ -0,0 +1,25 @@
+import {Component} from 'angular2/core';
+import 'rxjs/add/operator/map'; // crappy shim
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+import {Contact, ContactComponent} from '../contact/contact.component';
+
+@Component({
+    selector: 'contact-list',
+    templateUrl: 'app/contact-list/contact-list.component.html',
+    styleUrls: ['app/contact-list/contact-list.component.css'],
+    viewProviders: [HTTP_PROVIDERS],
+    directives: [ContactComponent]
+})
+export class ContactList {
+  contacts: Contact[] = new Array<Contact>()
+  constructor(http: Http) {
+    http.get('seed.json')
+    .map(res => res.json())
+    .subscribe((contacts : {contacts: Contact[]}) => {
+      for (let c of contacts.contacts) {
+        this.contacts.push(c)
+      }
+      this.contacts = this.contacts.sort((c: Contact, o: Contact) => c.name > o.name ? 1 : 0)
+    })
+  }
+}
